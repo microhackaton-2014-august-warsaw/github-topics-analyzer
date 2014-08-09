1ca8827eb964c610818513fcc30cfc197a3f74c7
Mikołaj Olszewski <mikus156@gmail.com>
2014-08-09 13:40:45 +0200

    optional input processing

--- ghtopanal/views.py
+++ ghtopanal/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponseBadRequest
 
 from .tasks import *
 
 @require_http_methods(["POST"])
 def analyze(request, pair_id):
+    if not request.POST:
+        return HttpResponseBadRequest()
     process.delay(pair_id, request.POST)
-    return HttpResponse(pair_id, mimetype='text/plain')
+    return HttpResponse(status=202)
